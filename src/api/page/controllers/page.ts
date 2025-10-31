04c854fcd0eb315af57ea1b6342907ecd982c1af
Pavel Mishakov <pashamishakov@gmail.com>
2025-10-31 16:39:10 +0500

    add email service

--- src/api/page/controllers/page.ts
+++ src/api/page/controllers/page.ts
@@ -4,4 +4,20 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::page.page');
+export default factories.createCoreController('api::page.page', ({strapi}) => ({
+    async emailAction(ctx) {
+        const data = ctx.request.body
+        try {
+            await strapi.plugins['email'].services.email.send({
+                to: 'pashamishakov@gmail.com',
+                from: 'pashamishakov@gmail.com',
+                subject: 'New contact available',
+                text: JSON.stringify(data),
+            })
+        } catch(err) {
+            console.log(err)
+        }
+       
+        ctx.body = { message: 'Item created successfully', data };
+    }
+}));
---
+++ src/api/page/routes/01-custom.ts
@@ -0,0 +1,9 @@
+export default {
+    routes: [
+        {
+            method: 'POST',
+            path: '/page/contacts_email',
+            handler: 'page.emailAction'
+        }
+    ]
+}
